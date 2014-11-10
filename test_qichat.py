af71e644bd78e71353e6959298dfb03396f8c42e
ccann <cocanning11@gmail.com>
2014-11-10 11:26:15 -0500

    changes

--- test_qichat.py
+++ test_qichat.py
@@ -6,32 +6,37 @@ import os
 
 def main():
     try:
-        session = qi.Session('localhost')
-        dialog = session.service('ALDialog')
         try:
-            session.service('DialogUtilities').load_lexicon()
-        except RuntimeError:
-            print('Load Lexicon with DialogUtilities service failed...')
+            session = qi.Session('localhost')
+            dialog = session.service('ALDialog')
+        except RuntimeError as e:
+            raise RuntimeError("[ERROR] requires a local naoqi session...\n{}".format(e))
         try:
-            dialog.loadTopic(os.path.join(os.path.expanduser('~'),
-                                          '.local',
-                                          'share',
-                                          'PackageManager',
-                                          'apps',
-                                          'dialog_utilities',
-                                          'dialog',
-                                          'Lexicon',
-                                          'Lexicon.dlg'))
-        except:
-            print('Load Lexicon by path failed...')
+            result = session.service('DialogUtilities').load_lexicon()
+        except RuntimeError as e:
+            raise RuntimeError('Load Lexicon with DialogUtilities service failed...')
+        if result:
+            try:
+                dialog.loadTopic(os.path.join(os.path.expanduser('~'),
+                                              '.local',
+                                              'share',
+                                              'PackageManager',
+                                              'apps',
+                                              'dialog_utilities',
+                                              'dialog',
+                                              'Lexicon',
+                                              'Lexicon.dlg'))
+            except RuntimeError:
+                raise RuntimeError('Load Lexicon by path failed...')
 
-        dlg_path = sys.argv[1]
-        # print(dlg_path)
-        dialog.loadTopic(dlg_path)
-        print('SUCCESS')
-    except RuntimeError:
-        print("[ERROR] requires a local naoqi session...")
-
+            dlg_path = sys.argv[1]
+            # print(dlg_path)
+            dialog.loadTopic(dlg_path)
+            print('SUCCESS')
+        else:
+            print('Load Lexicon with DialogUtilities returned False...')
+    except RuntimeError as e:
+        print(e)
 
 if __name__ == '__main__':
     main()
